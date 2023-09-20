6ef145385f47d1541ef56fd4f42b5afbad9e7289
Calm Dang <d.p.cuong2002@gmail.com>
2023-09-20 15:18:29 +0700

    add Testbench report

--- test/driver.cpp
+++ test/driver.cpp
@@ -1,4 +1,4 @@
-#define MAX_SIM 2000
+#define MAX_SIM 200
 
 void set_random(Vtop *dut, vluint64_t sim_unit) {
     dut->en_i = 1;
@@ -9,17 +9,30 @@ void set_random(Vtop *dut, vluint64_t sim_unit) {
     // Testbench
     static int sum = 0 ;
     static int test_counter = 0 ;
+    static int passed_test = 0 ;
+    static int failed_test = 0 ;
+
     if(dut->done_o) {
-        if(dut->sum_o == sum )
-            printf("Test %d : %d %d :  \033[0;32mPASSS\033[0m\n ",test_counter,sum,dut->sum_o) ;
-        else
-            printf("Test %d : %d %d :  \033[0;31mFAIL\033[0m\n ",test_counter,sum,dut->sum_o) ;
+        if(dut->sum_o == sum ){
+            printf("Test %d : %d %d :  \033[0;32mPASSSED\033[0m\n ",test_counter,sum,dut->sum_o) ;
+            passed_test ++ ;
+        }
+        else {
+            printf("Test %d : %d %d :  \033[0;31mFAILED\033[0m\n ",test_counter,sum,dut->sum_o) ;
+            failed_test ++ ;
+        }
         sum = 0 ;
         test_counter ++ ;
     } else {
         sum += dut->data_i ;
     }
 
+    if(sim_unit == MAX_SIM-1){
+        printf("\n--------REPORT ------------------- \n") ;
+        printf("\033[0;32mPASSED : %d/%d\033[0m\n",passed_test,test_counter) ;
+        printf("\033[0;31mFAILED : %d/%d\033[0m\n",failed_test,test_counter) ;
+        printf("----------------------------------\n");
+    }
 }
 
 // void testbench(Vtop *dut){
